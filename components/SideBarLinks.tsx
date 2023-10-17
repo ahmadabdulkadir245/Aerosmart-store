023d95bbff44d882d2c65b3b0f699e445d530b25
ahmadabdulkadir245 <ahmadabdulkadir245@gmail.com>
2023-10-17 03:42:02 -0700

    fixed bugs

--- components/SideBarLinks.tsx
+++ components/SideBarLinks.tsx
@@ -1,73 +1,66 @@
-"use client"
-import { AiOutlineShop } from 'react-icons/ai'
-import { RiUser3Line } from 'react-icons/ri'
-import { GiConcreteBag, GiOpeningShell, GiWoodBeam } from 'react-icons/gi'
-import SideBarLink from './SideBarLink'
-import { MdAdminPanelSettings, MdConstruction } from 'react-icons/md'
-import {  AiOutlineShoppingCart} from "react-icons/ai";
-import { BsBricks } from 'react-icons/bs'
-import { useRecoilState } from 'recoil'
-import { navState } from '../atoms/navHandler'
-import { useRouter } from 'next/navigation'
-import { AuthContext } from '../context/authContext'
-import { useContext, useState } from 'react'
-import Loading from './Loading'
+import React, { useState } from 'react';
+import { AiOutlineShop } from 'react-icons/ai';
+import { RiUser3Line } from 'react-icons/ri';
+import { GiConcreteBag, GiOpeningShell, GiWoodBeam } from 'react-icons/gi';
+import SideBarLink from './SideBarLink';
+import { MdAdminPanelSettings, MdConstruction } from 'react-icons/md';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { BsBricks } from 'react-icons/bs';
+import { useRouter } from 'next/router';
+import { AuthContext } from '../context/authContext';
 
 function SideBarLinks() {
-  const [openSideBar, setOpenSideBar] = useRecoilState(navState);
-  const [loading, setLoading] = useState(false)
-  const router = useRouter()
-  const closeNav = (path) => {
-    setOpenSideBar(false)
-    router.push(`${path}`)
-  }
-  const {logout, authToken} = useContext(AuthContext)
-  const logoutHandler = () => {
-    logout()
-      router.replace('/login')
-    setOpenSideBar(false)
-  }
+  const [openSideBar, setOpenSideBar] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
+  // const { logout, authToken } = useContext(AuthContext);
 
-  if(loading) {
-    return <Loading/>
-  }
+  const closeNav = (path: string) => {
+    setOpenSideBar(false);
+    router.push(path);
+  };
+
+  const logoutHandler = () => {
+    // logout();
+    router.replace('/login');
+    setOpenSideBar(false);
+  };
 
   return (
-    <div className='px-[10px] py-[6px] pb-8 text-gray-500 capitalize text-md h-full overflow-y-scroll'>
-          {authToken ? 
-          <button className='rounded-md border-2 border-gray-400 py-2 w-[96%]' onClick={logoutHandler} >
-          logout
+    <div className="px-[10px] py-[6px] pb-8 text-gray-500 capitalize text-md h-full overflow-y-scroll">
+      {false ? (
+        <button className="rounded-md border-2 border-gray-400 py-2 w-[96%]" onClick={logoutHandler}>
+          Logout
         </button>
-        :
-            <div className='flex justify-between '>
-        <button className='rounded-md border-2 border-gray-400 py-2 w-[47%]' onClick={closeNav.bind(this, '/login')} >
-              Login
-            </button> 
-            <button className='rounded-md border-2 border-gray-400 py-2 w-[47%]' onClick={closeNav.bind(this, '/signup')}>
-              Sign Up
-              </button>
+      ) : (
+        <div className="flex justify-between">
+          <button className="rounded-md border-2 border-gray-400 py-2 w-[47%]" onClick={() => closeNav('/login')}>
+            Login
+          </button>
+          <button className="rounded-md border-2 border-gray-400 py-2 w-[47%]" onClick={() => closeNav('/signup')}>
+            Sign Up
+          </button>
         </div>
-            }
-        <hr className='h-[1px] w-full mt-4 mb-2 bg-gray-500 '/>
+      )}
+      <hr className="h-[1px] w-full mt-4 mb-2 bg-gray-500" />
 
-
-        <SideBarLink Icon={RiUser3Line} title={'my account'} path={'/account'}/>
-        <SideBarLink Icon={AiOutlineShop} title={'orders'} path={'/orders'}/>
-        <SideBarLink Icon={AiOutlineShoppingCart} title={'cart'} path={'/cart'}/>
-        <SideBarLink Icon={GiWoodBeam} title={'woods'} path={'/'}/>
-        <SideBarLink Icon={MdConstruction} title={'metals'} path={'/'}/>
-        <SideBarLink Icon={GiConcreteBag} title={'cement'} path={'/'}/>
-        <SideBarLink Icon={GiOpeningShell} title={'stones'} path={'/'}/>
-        <SideBarLink Icon={BsBricks} title={'bricks'} path={'/'}/>
-        {authToken &&
+      <SideBarLink Icon={RiUser3Line} title={'my account'} path={'/account'} />
+      <SideBarLink Icon={AiOutlineShop} title={'orders'} path={'/orders'} />
+      <SideBarLink Icon={AiOutlineShoppingCart} title={'cart'} path={'/cart'} />
+      <SideBarLink Icon={GiWoodBeam} title={'woods'} path={'/'} />
+      <SideBarLink Icon={MdConstruction} title={'metals'} path={'/'} />
+      <SideBarLink Icon={GiConcreteBag} title={'cement'} path={'/'} />
+      <SideBarLink Icon={GiOpeningShell} title={'stones'} path={'/'} />
+      <SideBarLink Icon={BsBricks} title={'bricks'} path={'/'} />
+      {false && (
         <>
-        <SideBarLink Icon={BsBricks} title={'admin products'} path={'/admin/products'}/>
-        <SideBarLink Icon={MdAdminPanelSettings} title={'add product'} path={'/admin/add-product'}/>
-        <SideBarLink Icon={MdAdminPanelSettings} title={'add banner image'} path={'/admin/add-banner'}/>
+          <SideBarLink Icon={BsBricks} title={'admin products'} path={'/admin/products'} />
+          <SideBarLink Icon={MdAdminPanelSettings} title={'add product'} path={'/admin/add-product'} />
+          <SideBarLink Icon={MdAdminPanelSettings} title={'add banner image'} path={'/admin/add-banner'} />
         </>
-      }
+      )}
     </div>
-  )
+  );
 }
 
-export default SideBarLinks
+export default SideBarLinks;
