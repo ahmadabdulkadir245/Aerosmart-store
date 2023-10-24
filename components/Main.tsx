f4e824d7a295a76c641431b2d15e00a97e0bbf16
ahmadabdulkadir245 <ahmadabdulkadir245@gmail.com>
2023-10-24 09:14:31 -0700

    added product category in home page

--- components/Main.tsx
+++ components/Main.tsx
@@ -2,6 +2,7 @@ import About from "./About";
 import { Product } from "@/types";
 import ProductSlider from "./ProductSlider";
 import ProductList from "./product-list";
+import DesktopCategory from "./DesktopCategory";
 
 
 interface MainProps {
@@ -15,6 +16,7 @@ interface MainProps {
   }) => {
   return (
     <>
+    <DesktopCategory />
       <About />
       <ProductSlider sectionTitle={'latest products'} products={products.slice(0, 10)} path={'/'} discount={false}/>
       <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
