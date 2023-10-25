49f38d377207fb6ae72b62b548e80194c97d00a5
ahmadabdulkadir245 <ahmadabdulkadir245@gmail.com>
2023-10-25 04:37:14 -0700

    removed wrong grid

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -25,7 +25,7 @@ const CartPage = () => {
   return (
     <div className="bg-white">
       <Container>
-      <div className='pt-5 lg:pt-10 lg:grid grid-cols-3 m-auto  lg:max-w-7xl max-h-[calc(100vh-132px)] lg:h-full transition-all duration-500 ease-in overflow-y-scroll scrollbar-hide'>
+      <div className='pt-5 lg:pt-10 m-auto  lg:max-w-7xl max-h-[calc(100vh-132px)] lg:h-full transition-all duration-500 ease-in overflow-y-scroll scrollbar-hide'>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-6 ">
             <div className="lg:col-span-7">
               <div className="hidden lg:flex items-center uppercase bg-gray-200 py-2 px-5 text-gray-800 text-sm font-poppins tracking-wider text-center">
