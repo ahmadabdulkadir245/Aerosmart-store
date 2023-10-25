639ab8fc9ac7e16c6c13c7e251b07cc1e11b43dc
ahmadabdulkadir245 <ahmadabdulkadir245@gmail.com>
2023-10-25 07:54:45 -0700

    adding more sliders

--- components/Main.tsx
+++ components/Main.tsx
@@ -14,11 +14,14 @@ interface MainProps {
     title,
     products
   }) => {
+    const reversedProducts = products.reverse();
   return (
     <>
     <DesktopCategory />
       <About />
       <ProductSlider sectionTitle={'latest products'} products={products.slice(0, 10)} path={'/'} discount={false}/>
+      <ProductSlider sectionTitle={'discount products'} products={reversedProducts.slice(0, 10)} path={'/'} discount={false}/>
+      <ProductSlider sectionTitle={'featured products'} products={products.slice(0, 10)} path={'/'} discount={false}/>
       <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
