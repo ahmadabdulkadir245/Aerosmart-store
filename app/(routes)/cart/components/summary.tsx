08d9f3f67ddcf307a88da93133262dd0d7abba51
ahmadabdulkadir245 <ahmadabdulkadir245@gmail.com>
2023-10-25 01:36:57 -0700

    fixed design in cart page

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -8,6 +8,7 @@ import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
+import { TbCurrencyNaira } from "react-icons/tb";
 
 const Summary = () => {
   const searchParams = useSearchParams();
@@ -38,9 +39,10 @@ const Summary = () => {
   }
 
   return ( 
-    <div
+    <>
+    {/* <div
       className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
-    >
+      >
       <h2 className="text-lg font-medium text-gray-900">
         Order summary
       </h2>
@@ -53,7 +55,29 @@ const Summary = () => {
       <Button onClick={onCheckout} disabled={items.length === 0} className="w-full mt-6">
         Checkout
       </Button>
-    </div>
+    </div> */}
+    <div className="">
+            <div className="hidden lg:flex items-center uppercase bg-gray-200 py-2 px-5 text-gray-700 text-sm font-poppins tracking-wider text-center">
+        <div className="text-sm">
+          <p><span className='hidden lg:inline-block'>ORDER SUMMERY</span> </p>
+        </div>
+
+            </div>
+            <div className="flex justify-between px-8 pt-6 lg:pt-6  ">
+            <p > Products:</p>
+            <p className="flex items-center font-changa">{items.length}</p>
+            </div>
+            <div className="flex justify-between px-8 py-4  ">
+            <p >Subtotal:</p>
+            <p className="flex items-center"><TbCurrencyNaira  className="w-5 h-5"/><p className="font-changa">{totalPrice.toLocaleString()}</p></p>
+            </div>
+          <div className="hidden  px-8 shadow-xl w-full    pb-2 overflow-hiddentext-gray-500  lg:block">
+      <button className="capitalize w-full h-[48px] rounded-md text-white  text-sm bg-yellow-500  mb-2 flex items-center justify-center m-auto hover:bg-yellow-400 transition-all delay-100 ease-in font-changa" 
+      onClick={onCheckout}>Go To Checkout | <TbCurrencyNaira  className="w-5 h-5"/> {totalPrice.toLocaleString()}
+      </button>
+          </div>
+          </div>
+      </>
   );
 }
  
